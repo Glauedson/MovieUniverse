6d14dc1b58f21e9b6c7c405715c88125388787cb
Glauedson <glauedson18s@gmail.com>
2025-06-02 15:35:15 -0300

    feat: Adiciona a funcionalidade completa da navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,29 @@
 // Header =  cabeçalho do site
 // Talvez eu coloque mais informações aqui no futuro, mas por enquanto é só um cabeçalho simples
 // sem links ou botões
+import { useState } from 'react'
 import style from './header.module.css'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faMagnifyingGlass, faTimes } from "@fortawesome/free-solid-svg-icons"
 
-export default function header() {
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className={style.header}>
         <div className={style.headerContent}>
 
             {/* menu hamburguer */}
             <FontAwesomeIcon
-            icon={faBars}
-            style={{ color: "#fcfcfc" }}
-            className={style.menuIcon}
+              icon={isMenuOpen ? faTimes : faBars}
+              style={{ color: "#fcfcfc" }}
+              className={style.menuIcon}
+              onClick={toggleMenu}
             />
 
             <div className={style.headerCategory}>
@@ -29,18 +37,27 @@ export default function header() {
 
             {/* Lupa */}
             <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            style={{ color: "#fcfcfc" }}
-            className={style.menuIcon}
+              icon={faMagnifyingGlass}
+              style={{ color: "#fcfcfc" }}
+              className={style.menuIcon}
             />
-
             <img 
               src="https://pbs.twimg.com/profile_images/1374017044266557440/aggBOWBw_400x400.jpg" 
               alt="perfil da pessoa"
               className={style.profileImage}
             />
-            
+
         </div>
+        
+        {/* Mobile Navigation */}
+        <nav className={`${style.mobileNav} ${isMenuOpen ? style.mobileNavOpen : ''}`}>
+          <div className={style.mobileNavContent}>
+            <Link to="/" onClick={toggleMenu}>EM ALTA</Link>
+            <Link to="/" onClick={toggleMenu}>FILMES</Link>
+            <Link to="/" onClick={toggleMenu}>SERIES</Link>
+          </div>
+        </nav>
+        
     </div>
   )
 }
